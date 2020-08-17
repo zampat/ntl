af0b99947cd8995d86e949f9ccc9128851036b5f
giudig <47992054+giudig@users.noreply.github.com>
2020-08-17 17:56:37 +0200

    Update script action

--- ntl_download.sh
+++ ntl_download.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Script action:
-# Check and download all ITL Contributed CheckCommand plugins into PluginsContribDir/ folder
-# 
+# Check and download all "ITL Contributed CheckCommand" plugins into {PluginsContribDir}/ntl folder
+# If a plugin already exists, a backup copy of the old one is created.
 
 # NTL plugin temp download DIR
 WORK_DIR=/tmp/ns
 
-MONITORING_PLUGINS_CONTRIB_DIR="/neteye/shared/monitoring/plugins/"
+MONITORING_PLUGINS_CONTRIB_DIR="/neteye/shared/monitoring/plugins/ntl/"
 DATE=`date +%Y%m%d`
 
 #Define the NTL monitoring-plugins to copy into PluginsContribDir/
@@ -31,18 +31,18 @@ echo "[+] 099: install NTL monitoring plugins into PluginsContribDir/ [$MONITORI
 for PLUGIN in $(find $NTL_DIR -name '*.json');
 do
    PLUGIN_SRC=`/usr/bin/jq -r ."plugin_url" $PLUGIN`
-   echo "PLUGIN_SRC=$PLUGIN_SRC"
+   #echo "PLUGIN_SRC=$PLUGIN_SRC"
    PLUGIN_FILE=`/usr/bin/jq -r ."plugin_url" $PLUGIN | /usr/bin/rev | /usr/bin/cut -d / -f 1 | /usr/bin/rev`
-   echo "PLUGIN_FILE=$PLUGIN_FILE"
+   #echo "PLUGIN_FILE=$PLUGIN_FILE"
    DOWNLOADABLE=`/usr/bin/jq -r ."downloadable" $PLUGIN`
-   echo "DOWNLOADABLE=$DOWNLOADABLE"
+   #echo "DOWNLOADABLE=$DOWNLOADABLE"
    if [ $DOWNLOADABLE == "yes" ]
    then
         echo "[+] Copying Monitoring Plugin ${!PLUGIN_FILE} from git to Plugins contrib dir: ${MONITORING_PLUGINS_CONTRIB_DIR}"
         #wget $PLUGIN_SRC -O $PLUGIN_FILE -P $WORK_DIR
         cd $WORK_DIR
         wget $PLUGIN_SRC -O $PLUGIN_FILE
-        echo "WORK_DIR/PLUGIN_FILE=${WORK_DIR}/${PLUGIN_FILE}"
+        #echo "WORK_DIR/PLUGIN_FILE=${WORK_DIR}/${PLUGIN_FILE}"
 
         # Check if Plugin already exists. If yes: backup first
         if [ -f ${MONITORING_PLUGINS_CONTRIB_DIR}/${PLUGIN_FILE} ]
