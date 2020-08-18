051fa45a205d01075c926827f10f94b4e338845c
giudig <47992054+giudig@users.noreply.github.com>
2020-08-18 11:58:32 +0200

    Update comments

--- ntl_download.sh
+++ ntl_download.sh
@@ -7,11 +7,13 @@
 # NTL plugin temp download DIR
 WORK_DIR=/tmp/ns
 
-MONITORING_PLUGINS_CONTRIB_DIR="/neteye/shared/monitoring/plugins/ntl/"
-DATE=`date +%Y%m%d`
+# Define the PluginContribDir - standard path on NetEye 4 is /neteye/shared/monitoring/plugins/
+MONITORING_PLUGINS_CONTRIB_DIR="/neteye/shared/monitoring/plugins/"
 
 #Define the NTL monitoring-plugins to copy into PluginsContribDir/
 NTL_DIR=${WORK_DIR}/ntl/contributed_plugin_check_commands/
+
+DATE=`date +%Y%m%d`
 
 # Valiation: Check existency of folder PluginsContrib and WORK_DIR
 if [ ! -d "${MONITORING_PLUGINS_CONTRIB_DIR}" ]
@@ -30,19 +32,16 @@ echo "[+] 099: install NTL monitoring plugins into PluginsContribDir/ [$MONITORI
 
 for PLUGIN in $(find $NTL_DIR -name '*.json');
 do
+   #Explore json file using jq to identify plugin_url, plugin_file_name, downloadable field 
    PLUGIN_SRC=`/usr/bin/jq -r ."plugin_url" $PLUGIN`
-   #echo "PLUGIN_SRC=$PLUGIN_SRC"
    PLUGIN_FILE=`/usr/bin/jq -r ."plugin_url" $PLUGIN | /usr/bin/rev | /usr/bin/cut -d / -f 1 | /usr/bin/rev`
-   #echo "PLUGIN_FILE=$PLUGIN_FILE"
    DOWNLOADABLE=`/usr/bin/jq -r ."downloadable" $PLUGIN`
-   #echo "DOWNLOADABLE=$DOWNLOADABLE"
+   #The plugin download depends on downloadable field 
    if [ $DOWNLOADABLE == "yes" ]
    then
         echo "[+] Copying Monitoring Plugin ${!PLUGIN_FILE} from git to Plugins contrib dir: ${MONITORING_PLUGINS_CONTRIB_DIR}"
-        #wget $PLUGIN_SRC -O $PLUGIN_FILE -P $WORK_DIR
         cd $WORK_DIR
         wget $PLUGIN_SRC -O $PLUGIN_FILE
-        #echo "WORK_DIR/PLUGIN_FILE=${WORK_DIR}/${PLUGIN_FILE}"
 
         # Check if Plugin already exists. If yes: backup first
         if [ -f ${MONITORING_PLUGINS_CONTRIB_DIR}/${PLUGIN_FILE} ]
@@ -58,13 +57,11 @@ do
              echo "[+] Creating Backup of ${PLUGIN_FILE}"
              cp --force ${MONITORING_PLUGINS_CONTRIB_DIR}/${PLUGIN_FILE} ${MONITORING_PLUGINS_CONTRIB_DIR}/${PLUGIN_FILE}.${DATE}_bak
         fi
-        #cp ${!PLUGIN_SRC} ${MONITORING_PLUGINS_CONTRIB_DIR}
         cp ${WORK_DIR}/${PLUGIN_FILE} ${MONITORING_PLUGINS_CONTRIB_DIR}
         echo "[i] Copied ${!PLUGIN_FILE}"
    else
         echo "[i] Plugin ${PLUGIN_FILE} not downloadable! Skipped!"
    fi
 done
-
 
 echo "[i] 099: Done."
